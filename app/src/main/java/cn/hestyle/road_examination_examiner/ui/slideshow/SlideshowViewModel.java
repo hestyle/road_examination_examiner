73eb49aba570727b5c21b3f621e0499aed2a359c
hestyle <hestyle@qq.com>
2020-12-12 19:38:29 +0800

    first commit

---
+++ app/src/main/java/cn/hestyle/road_examination_examiner/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package cn.hestyle.road_examination_examiner.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
