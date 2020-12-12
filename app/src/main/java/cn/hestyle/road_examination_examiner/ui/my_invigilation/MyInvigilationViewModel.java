9ae4b0adb93aa627a3506089cc3df5c1b7815480
hestyle <hestyle@qq.com>
2020-12-12 20:22:16 +0800

    fix：完成侧滑菜单“我的监考”修改

--- app/src/main/java/cn/hestyle/road_examination_examiner/ui/my_invigilation/MyInvigilationViewModel.java
+++ app/src/main/java/cn/hestyle/road_examination_examiner/ui/my_invigilation/MyInvigilationViewModel.java
@@ -1,16 +1,16 @@
-package cn.hestyle.road_examination_examiner.ui.home;
+package cn.hestyle.road_examination_examiner.ui.my_invigilation;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class MyInvigilationViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public MyInvigilationViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is my_invigilation fragment");
     }
 
     public LiveData<String> getText() {
